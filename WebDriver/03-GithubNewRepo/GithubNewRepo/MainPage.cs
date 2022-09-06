e5b233bdee839f89eccfee830d7c65e3abf2ac01
Qintarra <brain_damage@ukr.net>
2022-09-06 15:22:21 +0300

    pages

---
+++ WebDriver/03-GithubNewRepo/GithubNewRepo/MainPage.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+using System;
+
+namespace GitHubAutomation.Pages
+{
+    public class MainPage
+    {
+        private const string BASE_URL = "http://www.github.com/";
+
+        private readonly IWebDriver driver;
+
+        private IWebElement ButtonCreateNew => driver.FindElement(By.XPath("//summary[@aria-label='Create new…']"));
+
+        private IWebElement LinkNewRepository => driver.FindElement(By.XPath("//a[normalize-space()='New repository']"));
+
+        public MainPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void OpenPage()
+        {
+            driver.Navigate().GoToUrl(BASE_URL);
+        }
+
+        public void ClickOnCreateNewRepositoryButton()
+        {
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            ButtonCreateNew.Click();
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            LinkNewRepository.Click();
+        }
+    }
+}
