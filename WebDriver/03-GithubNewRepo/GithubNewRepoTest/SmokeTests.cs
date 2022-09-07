d1fe4d18900d30525b99370f9e9a54eedfb11d78
Qintarra <brain_damage@ukr.net>
2022-09-07 11:21:48 +0300

    tests

---
+++ WebDriver/03-GithubNewRepo/GithubNewRepoTest/SmokeTests.cs
@@ -0,0 +1,41 @@
+using GitHubAutomation.Steps;
+using NUnit.Framework;
+
+namespace github_auto
+{
+    public class SmokeTests
+    {
+        private readonly Steps steps = new Steps();
+        private const string USERNAME = "****"; 
+        private const string PASSWORD = "****"; 
+        private const int REPOSITORY_RANDOM_POSTFIX_LENGTH = 6;
+
+        [SetUp]
+        public void Init()
+        {
+            steps.InitBrowser();
+        }
+
+        [Test]
+        public void OneCanLoginGithub()
+        {
+            steps.LoginGithub(USERNAME, PASSWORD);
+            Assert.That(steps.GetLoggedInUserName(), Is.EqualTo(USERNAME));
+        }
+
+        [Test]
+        public void OneCanCreateProject()
+        {
+            steps.LoginGithub(USERNAME, PASSWORD);
+            string repositoryName = Steps.GenerateRandomRepositoryNameWithCharLength(REPOSITORY_RANDOM_POSTFIX_LENGTH);
+            steps.CreateNewRepository(repositoryName, "auto-generated test repo");
+            Assert.That(steps.GetCurrentRepositoryName(), Is.EqualTo(repositoryName));
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            Steps.CloseBrowser();
+        }
+    }
+}
